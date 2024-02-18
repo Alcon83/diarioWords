208dd4156281b23fde0a17c4ade63e4df3876bca
Rafa <ambaron@gmail.com>
2024-02-18 18:38:44 +0100

    cambios para vercel con server  render problemas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Link from "next/link";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
-import { getServerSession } from "next-auth/next";
 import { FaHome, FaSignInAlt, FaSignOutAlt, FaUserPlus, FaSpaceShuttle } from 'react-icons/fa';
 
-async function Footer() {
-  const session = await getServerSession(authOptions);
+function Footer() {
+
   return (
     <footer className="bg-gray-800 text-white px-6 py-8">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
@@ -15,29 +13,29 @@ async function Footer() {
         </div>
 
         <ul className="flex gap-x-4 mb-6 md:mb-0">
-          {!session?.user ? (
-            <>
-              <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
-                <Link href="/"><FaHome className="text-lg" /></Link>
-              </li>
-              <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
-                <Link href="/auth/login"><FaSignInAlt className="text-lg" /></Link>
-              </li>
-              <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
-                <Link href="/auth/register"><FaUserPlus className="text-lg" /></Link>
-              </li>
-            </>
-          ) : (
-            <>
 
-              <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
-                <Link href="/dashboard"><FaHome className="text-lg" /></Link>
-              </li>
-              <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
-                <Link href="/api/auth/signout"> <FaSignOutAlt className="text-lg" /></Link>
-              </li>
-            </>
-          )}
+          <>
+            <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
+              <Link href="/"><FaHome className="text-lg" /></Link>
+            </li>
+            <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
+              <Link href="/auth/login"><FaSignInAlt className="text-lg" /></Link>
+            </li>
+            <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
+              <Link href="/auth/register"><FaUserPlus className="text-lg" /></Link>
+            </li>
+          </>
+
+          <>
+
+            <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
+              <Link href="/dashboard"><FaHome className="text-lg" /></Link>
+            </li>
+            <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
+              <Link href="/api/auth/signout"> <FaSignOutAlt className="text-lg" /></Link>
+            </li>
+          </>
+
         </ul>
 
         <div className="flex gap-x-4">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+
 import { FaHome, FaSignInAlt, FaSignOutAlt, FaUserPlus, FaSpaceShuttle } from 'react-icons/fa';
 
-async function Navbar() {
-  const session = await getServerSession(authOptions);
-  console.log(session);
+ function Navbar() {
 
   return (
     <nav className="flex justify-between items-center bg-gray-800 text-white px-6 py-4">
@@ -14,7 +12,7 @@ async function Navbar() {
       <Link href="/" className="text-3xl font-bold">TuDiario.</Link>
       </div>
       <ul className="flex gap-x-4">
-        {!session?.user ? (
+      
           <>
             <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
               <Link href="/"><FaHome className="text-lg" /></Link>
@@ -26,7 +24,7 @@ async function Navbar() {
               <Link href="/auth/register"><FaUserPlus className="text-lg" /></Link>
             </li>
           </>
-        ) : (
+
           <>
 
             <li className="hover:text-blue-400 transition-colors duration-300 cursor-pointer">
@@ -36,7 +34,7 @@ async function Navbar() {
               <Link href="/api/auth/signout"> <FaSignOutAlt className="text-lg" /></Link>
             </li>
           </>
-        )}
+
       </ul>
     </nav>
   );
