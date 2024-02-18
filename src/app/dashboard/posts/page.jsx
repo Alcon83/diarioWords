52dbb3029456c3621b179ccaddf90b3af8eba94c
Rafa <ambaron@gmail.com>
2024-02-18 17:11:03 +0100

    cambios para vercel

--- src/app/dashboard/posts/page.jsx
+++ src/app/dashboard/posts/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import PostCard from '@/components/PostCard'; // Asegúrate de ajustar la ruta de importación según tu estructura
+import PostCard from '/components/PostCard'; // Asegúrate de ajustar la ruta de importación según tu estructura
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { FaPlus } from 'react-icons/fa'
